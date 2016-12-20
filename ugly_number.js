e30b05c4b6033d15f7fc6b7a9d957887fa423a6d
Robert Schneiderman <robert.a.schneiderman@gmail.com>
2016-12-19 22:11:39 -0500

    kth largest number

---
+++ ugly_number.js
@@ -0,0 +1,24 @@
+// Write a program to check whether a given number is an ugly number.
+
+// Ugly numbers are positive numbers whose prime factors only include 2, 3, 5. For example, 6, 8 are ugly while 14 is not ugly since it includes another prime factor 7.
+
+// Note that 1 is typically treated as an ugly number.
+
+
+const isUgly = num => {
+    if (num === 1) return true;
+
+    for (factor = num; factor > 5; factor--) {
+        if (isPrimeAndAboveFive(factor)) return false;
+    }
+    return true;
+}
+
+const isPrimeAndAboveFive = num => {
+    for (div = num-1; div > 5; div--) {
+        if (num % div === 0) return false;
+    }
+    return true;
+}
+
+console.log('isUgly(7): ', isUgly(7));
